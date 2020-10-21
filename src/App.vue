2dbd167bdb55d94cb904f17118fb7bc0944015f8
Sebastián Segura Osorio <sseguraosorio@gmail.com>
2020-10-21 14:52:04 -0300

    first commit , layout & components ready

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,90 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<v-card class="overflow-hidden">
+    <v-app-bar absolute color="white" elevate-on-scroll scroll-target="#scrolling-techniques-7">
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Rick and morty</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+        <p class="navbar-link">Inicio</p>
+
+        <p class="navbar-link">Buscar</p>
+
+        <p class="navbar-link">Acerca de</p>
+    </v-app-bar>
+
+    <v-sheet id="scrolling-techniques-7" class="overflow-y-auto main" min-height="100vh">
+        <v-container fluid>
+            <v-row no-gutters>
+                <Card v-for="personaje in personajes" :key="personaje.id" :id="personaje.id" :nombre="personaje.nombre" :imagen="personaje.imagen" />
+            </v-row>
+        </v-container>
+    </v-sheet>
+</v-card>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from "./components/Card";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    name: "App",
+
+    components: {
+        Card,
+    },
+
+    data: () => ({
+        personajes: [{
+                id: 1,
+                nombre: "Morty",
+                imagen: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+            },
+            {
+                id: 2,
+                nombre: "Rick",
+                imagen: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+            },
+            {
+                id: 3,
+                nombre: "Summer",
+                imagen: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+            },
+            {
+                id: 4,
+                nombre: "Beth",
+                imagen: "https://rickandmortyapi.com/api/character/avatar/4.jpeg",
+            },
+            {
+                id: 5,
+                nombre: "Jerry",
+                imagen: "https://rickandmortyapi.com/api/character/avatar/5.jpeg",
+            },
+        ],
+    }),
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+.main {
+    padding-top: 80px;
+}
+
+.navbar-link {
+    text-decoration: none;
+    color: #000;
+    padding: 0 10px;
+
+    &:hover {
+        border-bottom: 2px solid #000;
+        cursor: pointer;
+    }
+
+    &:active {
+        border-bottom: 2px solid #000;
+    }
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,29 @@
+<template>
+<v-col cols="12" xs="6" sm="4" align="center">
+    <div class="wrapper">
+        <v-card max-width="350">
+            <v-img :src="imagen"></v-img>
+
+            <div class="card-body">
+                <v-card-title> {{ id }} - {{ nombre }} </v-card-title>
+            </div>
+        </v-card>
+    </div>
+</v-col>
+</template>
+
+<script>
+export default {
+    name: "Card",
+    props: ["id", "nombre", "imagen"],
+    data: () => ({
+        show: false,
+    }),
+};
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+    padding: 20px;
+}
+</style>
